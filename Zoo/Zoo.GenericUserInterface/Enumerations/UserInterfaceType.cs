d3628ccccb3000df1db9bc9ddc0a76effba649fd
Дмитрий Сердюков <dimaserd84@gmail.com>
2019-12-28 14:05:25 +0300

    Update Packs

--- Zoo/Zoo.GenericUserInterface/Enumerations/UserInterfaceType.cs
+++ Zoo/Zoo.GenericUserInterface/Enumerations/UserInterfaceType.cs
@@ -1,12 +1,43 @@
 ﻿namespace Zoo.GenericUserInterface.Enumerations
 {
+    /// <summary>
+    /// Тип пользовательского интерейса для свойства
+    /// </summary>
     public enum UserInterfaceType
     {
+        /// <summary>
+        /// Кастомный элемент ввода
+        /// </summary>
+        CustomInput,
+
+        /// <summary>
+        /// Текстовое поле
+        /// </summary>
         TextBox,
+
+        /// <summary>
+        /// Большое текстовое поле
+        /// </summary>
         TextArea,
+
+        /// <summary>
+        /// Выпадающий список
+        /// </summary>
         DropDownList,
+
+        /// <summary>
+        /// Скрытый инпут со значением
+        /// </summary>
         Hidden,
+
+        /// <summary>
+        /// Календарь для даты
+        /// </summary>
         DatePicker,
+
+        /// <summary>
+        /// Выпадающий список с множественным выбором
+        /// </summary>
         MultipleDropDownList,
     }
 }
--- Zoo/Zoo.GenericUserInterface/Models/UserInterfaceBlock.cs
+++ Zoo/Zoo.GenericUserInterface/Models/UserInterfaceBlock.cs
@@ -3,13 +3,29 @@ using Zoo.GenericUserInterface.Enumerations;
 
 namespace Zoo.GenericUserInterface.Models
 {
-
+    /// <summary>
+    /// Блок описывающий свойство в модели
+    /// </summary>
     public class UserInterfaceBlock
     {
+        /// <summary>
+        /// Название свойства
+        /// </summary>
         public string PropertyName { get; set; }
 
+        /// <summary>
+        /// Тип пользовательского интрефейса
+        /// </summary>
         public UserInterfaceType InterfaceType { get; set; }
 
+        /// <summary>
+        /// Значения для выпадающего списка
+        /// </summary>
         public List<MySelectListItem> SelectList { get; set; }
+
+        /// <summary>
+        /// Json с кастомными данными, можно использовать для построения кастомного инпута
+        /// </summary>
+        public string CustomDataJson { get; set; }
     }
 }
