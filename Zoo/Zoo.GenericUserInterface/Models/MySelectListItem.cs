53c5338dccda718810f8a79c551eaeba4791f76a
Дмитрий Сердюков <dimaserd84@gmail.com>
2019-12-29 15:43:41 +0300

    Minor

--- Zoo/Zoo.GenericUserInterface/Models/MySelectListItem.cs
+++ Zoo/Zoo.GenericUserInterface/Models/MySelectListItem.cs
@@ -1,11 +1,23 @@
 ﻿namespace Zoo.GenericUserInterface.Models
 {
+    /// <summary>
+    /// Элемент выпадающего списка
+    /// </summary>
     public class MySelectListItem
     {
+        /// <summary>
+        /// Значение
+        /// </summary>
         public string Value { get; set; }
 
+        /// <summary>
+        /// Текст для показа
+        /// </summary>
         public string Text { get; set; }
 
+        /// <summary>
+        /// Флаг выбранности
+        /// </summary>
         public bool Selected { get; set; }
     }
 }
