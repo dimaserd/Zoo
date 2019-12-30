d346ac242a77f7014204536ba232852e90bdcc05
Дмитрий Сердюков <dimaserd84@gmail.com>
2019-12-30 23:02:54 +0300

    Fixes

--- Zoo.GenericUserInterface.Tests/NullableEnumTests.cs
+++ Zoo.GenericUserInterface.Tests/NullableEnumTests.cs
@@ -1,8 +1,5 @@
 ﻿using NUnit.Framework;
-using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using Zoo.GenericUserInterface.Models;
 using Zoo.GenericUserInterface.Services;
 
--- Zoo/Zoo.GenericUserInterface/Models/Values/GenericUserInterfacePropertyGenericValue.cs
+++ Zoo/Zoo.GenericUserInterface/Models/Values/GenericUserInterfacePropertyGenericValue.cs
@@ -1,9 +1,19 @@
 ﻿namespace Zoo.GenericUserInterface.Models.Values
 {
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
     public class GenericUserInterfacePropertyGenericValue<T>
     {
+        /// <summary>
+        /// Название свойства
+        /// </summary>
         public string PropertyName { get; set; }
 
+        /// <summary>
+        /// Значение
+        /// </summary>
         public T Value { get; set; }
     }
 }
