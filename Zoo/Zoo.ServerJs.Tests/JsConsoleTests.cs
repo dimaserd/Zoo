f48d0577599fe2288f289e21e43b0304b4b202e1
dimaserd84@gmail.com <dimaserd84@gmail.com>
2020-11-20 20:49:44 +0300

    Minor

--- Zoo/Zoo.ServerJs.Tests/JsConsoleTests.cs
+++ Zoo/Zoo.ServerJs.Tests/JsConsoleTests.cs
@@ -2,6 +2,8 @@
 using System.Threading.Tasks;
 using Microsoft.Extensions.DependencyInjection;
 using NUnit.Framework;
+using Zoo.ServerJs.Models;
+using Zoo.ServerJs.Models.Method;
 using Zoo.ServerJs.Services;
 
 namespace Zoo.ServerJs.Tests
@@ -21,15 +23,56 @@ namespace Zoo.ServerJs.Tests
 
             var result = await jsExecutor.RunScriptDetaiiled("console.log(null)");
 
+            AssertResult(result);
+        }
+
+        [Test]
+        public async Task ConsoleLogNullFromService()
+        {
+            var services = new ServiceCollection();
+            new JsExecutorBuilder(services)
+                .AddJsWorker(builder => 
+                {
+                    builder.SetWorkerName("Test")
+                        .SetDescription("Test")
+                        .AddMethodViaFunction(FuncWithNull, new JsWorkerMethodDocsOptions
+                        {
+                            MethodName = "NullFunc",
+                            Description = "Some description"
+                        });
+
+                    return builder.Build();
+                })
+                .Build();
+
+            var srvProvider = services.BuildServiceProvider();
+
+            var jsExecutor = srvProvider.GetRequiredService<JsExecutor>();
+
+            var script = "var res = api.Call('Test', 'NullFunc');\n";
+            script += "console.log(res);";
+
+            var result = await jsExecutor.RunScriptDetaiiled(script);
+
+            AssertResult(result);
+        }
+
+        private static void AssertResult(JsScriptExecutedResult result)
+        {
             Assert.IsTrue(result.IsSucceeded);
             Assert.AreEqual(1, result.ConsoleLogs.Count);
 
             var log = result.ConsoleLogs.First();
             Assert.AreEqual(1, log.SerializedVariables.Count);
-            
+
             var variable = log.SerializedVariables.First();
             Assert.AreEqual("null", variable.DataJson);
             Assert.IsNull(variable.TypeFullName);
+        }
+
+        private static object FuncWithNull()
+        {
+            return null;
         }
     }
 }
