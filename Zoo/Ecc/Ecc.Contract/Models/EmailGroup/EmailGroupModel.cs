6b0a237c36c0a2ddb0a3852766a19a57cc447109
dimaserd84@gmail.com <dimaserd84@gmail.com>
2021-04-26 17:48:51 +0300

    Fixes

--- Zoo/Ecc/Ecc.Contract/Models/EmailGroup/EmailGroupModel.cs
+++ Zoo/Ecc/Ecc.Contract/Models/EmailGroup/EmailGroupModel.cs
@@ -1,11 +1,23 @@
 ﻿namespace Ecc.Contract.Models.EmailGroup
 {
+    /// <summary>
+    /// Группа эмейлов
+    /// </summary>
     public class EmailGroupModel
     {
+        /// <summary>
+        /// Идентификатор
+        /// </summary>
         public string Id { get; set; }
 
+        /// <summary>
+        /// Название
+        /// </summary>
         public string Name { get; set; }
 
+        /// <summary>
+        /// Количество эмейлов
+        /// </summary>
         public int EmailsCount { get; set; }
     }
 }
