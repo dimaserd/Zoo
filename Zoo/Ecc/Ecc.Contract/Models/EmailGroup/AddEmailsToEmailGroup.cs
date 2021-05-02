04c9e35afe249d0efa01318b63c13668355ce8c2
dimaserd84@gmail.com <dimaserd84@gmail.com>
2021-05-02 13:34:32 +0300

    Fixes

--- Zoo/Ecc/Ecc.Contract/Models/EmailGroup/AddEmailsToEmailGroup.cs
+++ Zoo/Ecc/Ecc.Contract/Models/EmailGroup/AddEmailsToEmailGroup.cs
@@ -3,10 +3,19 @@ using System.ComponentModel.DataAnnotations;
 
 namespace Ecc.Contract.Models.EmailGroup
 {
+    /// <summary>
+    /// Добавить эмейлы в группу
+    /// </summary>
     public class AddEmailsToEmailGroup
     {
+        /// <summary>
+        /// Идентификатор группы эмейлов
+        /// </summary>
         public string EmailGroupId { get; set; }
 
+        /// <summary>
+        /// Эмейлы
+        /// </summary>
         [Required(ErrorMessage = "Нужно указать Email адреса")]
         public List<string> Emails { get; set; }
     }
