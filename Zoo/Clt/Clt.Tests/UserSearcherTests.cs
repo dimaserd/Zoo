312d37278d54d1c54b2eb808b3e30c68e635e571
dimaserd84@gmail.com <dimaserd84@gmail.com>
2021-07-04 11:03:18 +0300

    Minor

--- Zoo/Clt/Clt.Tests/UserSearcherTests.cs
+++ Zoo/Clt/Clt.Tests/UserSearcherTests.cs
@@ -1,20 +1,9 @@
 ﻿using Clt.Contract.Settings;
-using Clt.Logic;
-using Clt.Logic.Services.Account;
 using Clt.Logic.Services.Users;
-using Clt.Model;
-using Clt.Tests.Extensions;
-using Croco.Core.Application;
 using Croco.Core.Contract;
-using Croco.Core.Contract.Application.Common;
-using Croco.Core.Contract.Files;
 using Croco.Core.Extensions;
-using Croco.Core.Logic.DbContexts;
-using Croco.Core.Logic.Files;
 using Microsoft.Extensions.DependencyInjection;
 using NUnit.Framework;
-using System;
-using System.IO;
 using System.Threading.Tasks;
 
 namespace Clt.Tests
